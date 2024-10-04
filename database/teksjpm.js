203aa3b940bcf55c9a2e5b99378b85dee56c9d95
Idzhar803 <akununtukmasadepan1@gmail.com>
2024-10-04 08:40:26 +0700

    Add files via upload

---
+++ database/teksjpm.js
@@ -0,0 +1,55 @@
+*🚀 LIST PRODUK SLIMEXTZY*
+> RESELLER PANEL/SUBDOMAIN 15K
+> ADMIN PANEL FRESH 20K
+> OWNER PANEL 30K
+> DOMAIN my.id/biz.id/.xyz
+> PANEL RAM 1GB - UNLIMITED
+> JASA RENAME SC
+> JASA FIKS SC EROR
+> JASA INSTALL PANEL
+> JASA INSTALL THEMA
+> JASA FIKS NODE MERAH
+> JASA RUN IN BOT DI NO LU
+> SC INSTALL PANEL
+> SC CPANEL FULL BUTTON
+> SC SLIME V3.0 PRIVATE
+> SC SLIME V3.5 PRIVATE
+> SC SLIME V4.0 SAMPE V9.0
+*PRODUK/SC LAIN NYA TANYAKAN*
+━━━━━━━━━━━━━━━━━━━━━
+*🌐 READY VPS DIGITAL OCEAN*
+• RAM 2GB 1 CORE 30K
+• RAM 4GB 2 CORE 45K
+• RAM 8GB 4 CORE 60K
+• RAM 16GB 4 CORE 85K
+
+*NEGO BANYAK? NOGAR BOS !*
+*☁ Bonus Nya*
+_• Free Script Install Panel/Cpanel_
+_• Free Install Panel Pterodactyl_
+_• Free Req Website Namamu_
+_• Free Install Node & Wings_
+_• Free Egg Bot Wa/Mengkrep_
+_• Free Install Thema Khusus Ram 8-16_
+_• Garansi 15 Hari_ *(1x Replace)*
+_• Vps Aktif 30 Hari_
+_• Free Akses Vps JuiceSSH_
+━━━━━━━━━━━━━━━━━━━━━
+*🛍 READY AKUN DIGITAL OCEAN VCC*
+
+10 Droplets Vcc 2 Bulan 200K
+10 Droplets Vcc 1 Tahun 220K
+
+*STOK TERBATAS⚠️*
+━━━━━━━━━━━━━━━━━━━━━
+☎️ Contact person :
+WhatsApp¹ : wa.me/6283807735938 ON
+WhatsApp² : wa.me/6285718275389 ON
+WhatsApp³ : wa.me/6285715430424 ON
+Telegram : t.me/sl1mecrash
+━━━━━━━━━━━━━━━━━━━━━
+*⚡Link Group¹* https://chat.whatsapp.com/KcOsY5C3KzuD9PQ396vRfq
+*⚡Link Group²* https://chat.whatsapp.com/Dx1RMTnZ88h14kk45QOgQJ
+*⚡Link Saluran* https://whatsapp.com/channel/0029VaicyTbAjPXOdW7P9f2x
+
+> © 𝐒𝐥𝐢𝐦𝐞𝐱𝐓𝐳𝐲
